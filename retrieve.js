da75a07bc74da0ee71d30fbb1d130498f08b0570
Eralp Sahin <eralpsahin@me.com>
2019-07-27 23:31:29 -0400

    Add endpoint for usd, euro, btc, xau currencies

--- retrieve.js
+++ retrieve.js
@@ -1,7 +1,11 @@
 const fetch = require('node-fetch');
 const schedule = require('node-schedule');
+
 const usd = require('debug')('drh:usd');
 const euro = require('debug')('drh:euro');
+const xau = require('debug')('drh:xau');
+const btc = require('debug')('drh:btc');
+
 const redisClient = require('./redis-client');
 
 /**
@@ -27,5 +31,31 @@ schedule.scheduleJob('* * * * *', () => {
       const data = body.SeriesData;
       redisClient.set('euro', data[data.length - 1][1]);
       euro(data[data.length - 1][1]);
+    });
+});
+
+/**
+ * Schedule XAU/TRY exchange rate.
+ */
+schedule.scheduleJob('0 8 * * *', () => {
+  fetch('https://www.bloomberght.com/piyasa/refdata/gram-altin')
+    .then((res) => res.json())
+    .then((body) => {
+      const data = body.SeriesData;
+      redisClient.set('xau', data[data.length - 1][1]);
+      xau(data[data.length - 1][1]);
+    });
+});
+
+/**
+ * Schedule BTC/TRY exchange rate.
+ */
+schedule.scheduleJob('* * * * *', () => {
+  fetch('https://www.paribu.com/ticker')
+    .then((res) => res.json())
+    .then((body) => {
+      const data = body.BTC_TL;
+      redisClient.set('btc', data.lowestAsk);
+      btc(data.lowestAsk);
     });
 });
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,24 @@ const { getAsync } = require('../redis-client');
 const router = express.Router();
 
 /* GET USD/TRY Exchange. */
-router.get('/', async (req, res) => {
-  const usd = await getAsync('usd');
-  res.json({ usd });
+router.get('/usd', async (req, res) => {
+  const rate = await getAsync('usd');
+  res.json({ rate });
+});
+
+router.get('/btc', async (req, res) => {
+  const rate = await getAsync('btc');
+  res.json({ rate });
+});
+
+router.get('/euro', async (req, res) => {
+  const rate = await getAsync('euro');
+  res.json({ rate });
+});
+
+router.get('/xau', async (req, res) => {
+  const rate = await getAsync('xau');
+  res.json({ rate });
 });
 
 module.exports = router;
